94c635b693c9730c6d19688cc96d06ae41b3e3e4
Gabriel Messias <gamessiasjt@gmail.com>
2025-03-10 21:24:35 -0300

    Initial commit

---
+++ project-integrator-vii-back-end/src/main/java/org/projetointegrador/unifio/projectintegratorviibackend/ProjectIntegratorViiBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.projetointegrador.unifio.projectintegratorviibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIntegratorViiBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectIntegratorViiBackEndApplication.class, args);
+    }
+
+}
---
+++ project-integrator-vii-back-end/src/test/java/org/projetointegrador/unifio/projectintegratorviibackend/ProjectIntegratorViiBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projetointegrador.unifio.projectintegratorviibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectIntegratorViiBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
