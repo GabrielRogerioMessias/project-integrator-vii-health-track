6d4698a89883318e47d34207a81389fcbfc85973
Gabriel Messias <gamessiasjt@gmail.com>
2025-03-23 22:38:41 -0300

    feat:(api) creat repository for pressure and blood glucose

---
+++ project-integrator-vii-back-end/src/main/java/org/projetointegrador/unifio/projectintegratorviibackend/repositories/BloodGlucoseRepository.java
@@ -0,0 +1,7 @@
+package org.projetointegrador.unifio.projectintegratorviibackend.repositories;
+
+import org.projetointegrador.unifio.projectintegratorviibackend.models.BloodGlucose;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BloodGlucoseRepository extends JpaRepository<BloodGlucose, Integer> {
+}
---
+++ project-integrator-vii-back-end/src/main/java/org/projetointegrador/unifio/projectintegratorviibackend/repositories/PressureRepository.java
@@ -0,0 +1,7 @@
+package org.projetointegrador.unifio.projectintegratorviibackend.repositories;
+
+import org.projetointegrador.unifio.projectintegratorviibackend.models.Pressure;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PressureRepository extends JpaRepository<Pressure, Integer> {
+}
